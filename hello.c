6bed08039f0564f71e582c4df5a380d028858c90
ProDesignV2 <eoinpgogarty@gmail.com>
2019-01-28 14:48:25 +0000

    Fixed #5

--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@ int main(char *argc[], int argv){
 	int my_number = 5;
 	printf("Initial arguments was %d\nMy number is %d\n", argv, my_number);
 	// Here is your stupid comment
+	// One comment obvioulsy wasn't enough
 	return 0;
 }
