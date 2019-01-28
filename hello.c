67f3ba446441e163ca6dae87b763c9d35f717ec4
ProDesignV2 <eoinpgogarty@gmail.com>
2019-01-28 14:38:38 +0000

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,5 +3,6 @@
 int main(char *argc[], int argv){
 	int my_number = 5;
 	printf("Initial arguments was %d\nMy number is %d\n", argv, my_number);
+	// Why did it automatically suggest a pull request?
 	return 0;
 }
